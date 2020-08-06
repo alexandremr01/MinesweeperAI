03021094f82bb0388f02e14e58a701221f8ab7e8
vasconcelosdavi <davi.muniz41@gmail.com>
2020-08-06 11:54:36 -0300

    histogram fix

--- multi_evaluator.py
+++ multi_evaluator.py
@@ -18,8 +18,8 @@ def random_actor(state):
   return x, y
 
 size = 8
-NUM_EPISODES = 100
-bombs = [8, 12, 16]
+NUM_EPISODES = 10000
+bombs = [8, 10, 12]
 
 victories = 0
 plays_to_die = []
@@ -57,29 +57,29 @@ for current_game in range(num_games):
           victories += 1
         else:
           plays_to_die.append(plays-1)
-        open_percentage.append(game.get_open_percentage())
-        print('Game', current_game + 1, '/', num_games, 'Played',episodes,'/',NUM_EPISODES, "Open percentage:", game.get_open_percentage())
+        open_percentage.append(game.get_open_percentage()*100)
+        print('Game', current_game + 1, '/', num_games, 'Played',episodes,'/',NUM_EPISODES, 'Open percentage:', game.get_open_percentage()*100, '%')
     games_open_percentage.append(open_percentage)
     games_plays_to_die.append(plays_to_die)
     victory_percentage = victories / NUM_EPISODES
-    games_victory_percentage.append(victory_percentage)
+    games_victory_percentage.append(victory_percentage*100)
 
-print('Win rate:', bombs[0], 'bombs -', games_victory_percentage[0], '//', bombs[1], 'bombs -', games_victory_percentage[1], '//', bombs[2]
-, 'bombs -', games_victory_percentage[2])
+print('Win rate:', bombs[0], 'bombs -', games_victory_percentage[0], '%//', bombs[1], 'bombs -', games_victory_percentage[1], '%//', bombs[2]
+, 'bombs -', games_victory_percentage[2], '%')
 
 # Plots return history
 plt.hist(games_plays_to_die[0], bins=list(range(0,np.max(np.max(games_plays_to_die)))), label='8 Bombs', alpha=0.6, color='b')
-plt.hist(games_plays_to_die[1], bins=list(range(0,np.max(np.max(games_plays_to_die)))), label='12 Bombs', alpha=0.6, color='darkgreen')
-plt.hist(games_plays_to_die[2], bins=list(range(0,np.max(np.max(games_plays_to_die)))), label='16 Bombs', alpha=0.6, color='r')
+plt.hist(games_plays_to_die[1], bins=list(range(0,np.max(np.max(games_plays_to_die)))), label='10 Bombs', alpha=0.6, color='darkgreen')
+plt.hist(games_plays_to_die[2], bins=list(range(0,np.max(np.max(games_plays_to_die)))), label='12 Bombs', alpha=0.6, color='r')
 plt.legend(loc='upper right')
 plt.xlabel('# plays')
 plt.ylabel('# episodes')
 plt.title('Histogram of number of plays untill defeat')
 plt.show()
 
-plt.hist(games_open_percentage[0], label='8 Bombs', alpha=0.6, color='b')
-plt.hist(games_open_percentage[1], label='12 Bombs', alpha=0.6, color='darkgreen')
-plt.hist(games_open_percentage[2], label='16 Bombs', alpha=0.6, color='r')
+plt.hist(games_open_percentage[0], bins=20, label='8 Bombs', alpha=0.6, color='b')
+plt.hist(games_open_percentage[1], bins=20, label='10 Bombs', alpha=0.6, color='darkgreen')
+plt.hist(games_open_percentage[2], bins=20, label='12 Bombs', alpha=0.6, color='r')
 plt.legend(loc='upper left')
 plt.xlabel('% open')
 plt.ylabel('# episodes')
